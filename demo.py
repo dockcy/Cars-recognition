71876a4d29702e900b1ce254ba8377c381f8d722
dockcy <59227528+dockcy@users.noreply.github.com>
2020-05-10 14:44:59 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,247 @@
+#coding: UTF-8
+from __future__ import absolute_import  
+from __future__ import division  
+from __future__ import print_function  
+import colorsys
+import os
+from timeit import default_timer as timer
+import numpy as np
+from keras import backend as K
+from keras.models import load_model
+from keras.layers import Input
+from PIL import Image, ImageEnhance  
+from PIL import ImageFont, ImageDraw
+import random  
+import codecs  
+import sys  
+import functools  
+import math  
+import cv2 as cv
+#ShuffleNet车型分类需要PaddlePaddle架构
+
+import paddle  
+import paddle.fluid as fluid  
+from paddle.fluid import core  
+from paddle.fluid.param_attr import ParamAttr  
+
+#YOLOv3车辆检测需要用到keras架构
+from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
+from yolo3.utils import letterbox_image
+from keras.utils import multi_gpu_model
+
+
+class YOLO(object):
+    _defaults = {
+        "model_path": 'model_data/yolo_car.h5',
+        "anchors_path": 'model_data/yolo_anchors.txt',
+        "classes_path": 'model_data/coco_classes.txt',
+        "score" : 0.3,
+        "iou" : 0.45,
+        "model_image_size" : (416, 416),
+        "gpu_num" : 1,
+    }
+
+    @classmethod
+    def get_defaults(cls, n):
+        if n in cls._defaults:
+            return cls._defaults[n]
+        else:
+            return "Unrecognized attribute name '" + n + "'"
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(self._defaults) # set up default values
+        self.__dict__.update(kwargs) # and update with user overrides
+        self.class_names = self._get_class()
+        self.anchors = self._get_anchors()
+        self.sess = K.get_session()
+        self.boxes, self.scores, self.classes = self.generate()
+
+    def _get_class(self):
+        classes_path = os.path.expanduser(self.classes_path)
+        with open(classes_path) as f:
+            class_names = f.readlines()
+        class_names = [c.strip() for c in class_names]
+        return class_names
+
+    def _get_anchors(self):
+        anchors_path = os.path.expanduser(self.anchors_path)
+        with open(anchors_path) as f:
+            anchors = f.readline()
+        anchors = [float(x) for x in anchors.split(',')]
+        return np.array(anchors).reshape(-1, 2)
+
+    def generate(self):
+        model_path = os.path.expanduser(self.model_path)
+        assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
+
+        # Load model, or construct model and load weights.
+        num_anchors = len(self.anchors)
+        num_classes = len(self.class_names)
+        is_tiny_version = num_anchors==6 # default setting
+        try:
+            self.yolo_model = load_model(model_path, compile=False)
+        except:
+            self.yolo_model = tiny_yolo_body(Input(shape=(None,None,3)), num_anchors//2, num_classes) \
+                if is_tiny_version else yolo_body(Input(shape=(None,None,3)), num_anchors//3, num_classes)
+            self.yolo_model.load_weights(self.model_path) # make sure model, anchors and classes match
+        else:
+            assert self.yolo_model.layers[-1].output_shape[-1] == \
+                num_anchors/len(self.yolo_model.output) * (num_classes + 5), \
+                'Mismatch between model and given anchor and class sizes'
+
+        print('{} model, anchors, and classes loaded.'.format(model_path))
+
+        # Generate colors for drawing bounding boxes.
+        hsv_tuples = [(x / len(self.class_names), 1., 1.)
+                      for x in range(len(self.class_names))]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+        self.colors = list(
+            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+                self.colors))
+        np.random.seed(10101)  # Fixed seed for consistent colors across runs.
+        np.random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
+        np.random.seed(None)  # Reset seed to default.
+
+        # Generate output tensor targets for filtered bounding boxes.
+        self.input_image_shape = K.placeholder(shape=(2, ))
+        if self.gpu_num>=2:
+            self.yolo_model = multi_gpu_model(self.yolo_model, gpus=self.gpu_num)
+        boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors,
+                len(self.class_names), self.input_image_shape,
+                score_threshold=self.score, iou_threshold=self.iou)
+        return boxes, scores, classes
+    
+    
+    #图像检测
+    def detect_image(self, image):
+        #start = timer()
+        if self.model_image_size != (None, None):
+            assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
+            assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
+            boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
+        else:
+            new_image_size = (image.width - (image.width % 32),
+                              image.height - (image.height % 32))
+            boxed_image = letterbox_image(image, new_image_size)
+        image_data = np.array(boxed_image, dtype='float32') #转为浮点数才能送入网络运算
+
+        #加载shuffle_model
+        target_size = [3, 224, 224] 
+        mean_rgb = [127.5, 127.5, 127.5]
+        place = fluid.CUDAPlace(0)
+        exe = fluid.Executor(place)
+        save_freeze_dir = "./model_data/freeze-model"
+		
+        [inference_program, feed_target_names, fetch_targets] = fluid.io.load_inference_model(dirname=save_freeze_dir, executor=exe)  
+        
+        #设置框线及显示的字体
+        #font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+        font = ImageFont.truetype(font='simsun.ttc',
+                    size=np.floor(0.04*image.size[1]+0.5).astype('int32'))
+        thickness = (image.size[0] + image.size[1]) // 500
+        
+        #---------------YOLO的图像预处理----------
+        #print(image_data.shape)
+        image_data /= 255.
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.（m,width,height,Channels）
+        
+        #--------------shufflenet模型的输入图像预处理-------------
+        def crop_image(img, target_size):  
+            width, height = img.size  
+            w_start = (width - target_size[2]) / 2  
+            h_start = (height - target_size[1]) / 2  
+            w_end = w_start + target_size[2]  
+            h_end = h_start + target_size[1]  
+            img = img.crop((w_start, h_start, w_end, h_end))  
+            return img  
+        def resize_img(img, target_size):  
+            ret = img.resize((target_size[1], target_size[2]), Image.BILINEAR)  
+            return ret  
+        def read_image(img_path):  
+            img = img_path
+
+            if img.mode != 'RGB':  
+                img = img.convert('RGB')  
+            img = crop_image(img, target_size)  #将图改为输入的大小
+            img = np.array(img).astype('float32')  
+            img -= mean_rgb 
+            img = img.transpose((2, 0, 1))  # HWC to CHW  
+            img *= 0.007843
+            img = img[np.newaxis,:]  
+            return img
+        #---------------函数：使用shuffle_model预测分类-------------
+        def infer(image_path):  
+            tensor_img = read_image(image_path)  
+            label = exe.run(inference_program, feed={feed_target_names[0]: tensor_img}, fetch_list=fetch_targets)  
+            return np.argmax(label)  
+
+        #-------------------YOLO预测目标位置-------------
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+
+        print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
+
+        for i, c in reversed(list(enumerate(out_classes))):
+            predicted_class = self.class_names[c]
+            box = out_boxes[i]
+            score = out_scores[i]
+            draw = ImageDraw.Draw(image)
+            top, left, bottom, right = box
+			
+			
+			
+            top = max(0, np.floor(top + 0.5).astype('int32'))
+            left = max(0, np.floor(left + 0.5).astype('int32'))
+            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+            
+            #------------裁剪图像（注意PIL的crop函数需要使用元组的形式）----------------
+            image_crop = image.crop((left,top,right,bottom))
+
+            #-----------shuffle预测该图像的分类---------------
+            #预测出分类编号
+            result = infer(image_crop)
+            print(result)
+            #对应类别
+            label_file = "./label_list.txt"
+            with codecs.open(label_file,encoding = 'utf-8') as label_list:
+                labels = label_list.readlines()
+            label = labels[result].split('\t')[1]
+            label_list.close()
+            print(label, (left, top), (right, bottom))
+
+            label_size = draw.textsize(label, font)
+            if top - label_size[1] >= 0:
+                text_origin = np.array([left, top - label_size[1]])
+            else:
+                text_origin = np.array([left, top + 1])
+
+            # My kingdom for a good redistributable image drawing library.
+            for i in range(thickness):
+                draw.rectangle(
+                    [left + i, top + i, right - i, bottom - i],
+                    outline=self.colors[c])
+            
+            draw.rectangle(
+                [tuple(text_origin), tuple(text_origin + label_size)],
+                fill=self.colors[c])
+            draw.text(text_origin, label,fill=(0, 0, 0), font=font)
+            del draw
+
+        end = timer()
+        image.save('./results/{}.jpg'.format(end))
+        return image
+
+    def close_session(self):
+        self.sess.close()
+        
+if __name__ == '__main__': 
+    im_path  = test_files[0]
+    detect = YOLO();
+    detect.detect_image(Image.open(os.path.join(test_images,im_path)))
+
